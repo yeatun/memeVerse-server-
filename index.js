320c77d7b8fd865849b5de9e8ac946c0729c0f79
yeatun <p.yeatun@gmail.com>
2021-09-16 04:55:54 +0600

    fixed issuess

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const PORT = 3001;
 app.use(cors());
 app.use(express.json());
 // const port =process.env.PORT ||  3001;
