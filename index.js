1575620826e04faf236d6b7990cb1b6e1b6b78a7
yeatun <p.yeatun@gmail.com>
2021-09-15 19:03:14 +0600

    fix problems

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-
+const port =process.env.PORT ||  3001;
 const userRoute = require("./routes/User");
 app.use("/user", userRoute);
 const uploadRoute = require("./routes/Upload");
 app.use("/upload", uploadRoute);
 
+app.get('/', (req, res) => {
+  res.send("hello from db it's working ")
+})
 // app.listen(3001, (req, res) => {
 //   console.log("Server running...");
 // });
-app.listen(process.env.PORT || PORT, () =>{
-  console.log(`Server running... ${PORT}`);
-});
+// app.listen(process.env.PORT || PORT, () =>{
+//   console.log(`Server running... ${PORT}`);
+// });
+app.listen(process.env.PORT || port)
